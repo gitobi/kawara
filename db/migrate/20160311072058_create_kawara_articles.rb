d55f3e07446bb3faef88c5aa4903df6cb5719576
Rui Onodera <deraru@gmail.com>
2016-03-13 17:27:27 -0700

    Modify articles index

--- db/migrate/20160311072058_create_kawara_articles.rb
+++ db/migrate/20160311072058_create_kawara_articles.rb
@@ -4,9 +4,11 @@ class CreateKawaraArticles < ActiveRecord::Migration
       t.string :title
       t.string :subtitle
       t.text   :content
-      t.integer :status, default: 0, null: false, index: true
+      t.integer :status, default: 0, null: false
 
       t.timestamps null: false
     end
+
+    add_index :kawara_articles, [:status, :id]
   end
 end
