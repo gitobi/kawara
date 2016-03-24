c10e976e2f2b015cea442f3472e5586c40cf07f9
Rui Onodera <deraru@gmail.com>
2016-03-23 22:30:12 -0700

    Implement categories#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Kawara::Engine.routes.draw do
 
-  resources :articles, only: %w(show)
-  resources :tags,     only: %w(show)
+  resources :categories, only: %w(show)
+  resources :articles,   only: %w(show)
+  resources :tags,       only: %w(show)
   root 'home#index'
 end
