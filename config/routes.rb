0288b9a0b6ce7334b495fd8653a75d453767955b
Rui Onodera <deraru@gmail.com>
2016-03-26 23:30:53 -0700

    Implement tags#index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Kawara::Engine.routes.draw do
 
   resources :categories, only: %w(show)
   resources :articles,   only: %w(show)
-  resources :tags,       only: %w(show)
+  resources :tags,       only: %w(index show)
   root 'home#index'
 end
